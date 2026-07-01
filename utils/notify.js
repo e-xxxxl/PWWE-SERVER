19299ad032c24e210e0973baaa9bd62de6028863
emmanuel <eajejohnson@gmail.com>
2026-07-01 15:01:16 +0100

    updated schema name

--- utils/notify.js
+++ utils/notify.js
@@ -1,4 +1,4 @@
-const Notification = require('../models/Notification');
+const Notification = require('../models/notification');
 
 const notify = async (userId, { title, message, type = 'info' }) => {
   try {
